32ed952dca0e875882109e776d1cf4223e5e2a0a
webQbe <webcubesolutionslk@gmail.com>
2024-10-24 12:04:58 +0530

    Defined DOM Elements

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const CLIENT_ID = '610977770552-2reklne551tb7n57dohvfrksmcf865se.apps.googleuser
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
+// Select DOM Elements
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
