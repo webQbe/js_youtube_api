c44b9b50e68109587315db1782df54cefd24d0fd
webQbe <webcubesolutionslk@gmail.com>
2024-10-24 12:04:10 +0530

    Defined Options from API

---
+++ main.js
@@ -0,0 +1,5 @@
+// Options
+const CLIENT_ID = '610977770552-2reklne551tb7n57dohvfrksmcf865se.apps.googleusercontent.com';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
