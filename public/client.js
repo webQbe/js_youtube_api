d83de6a118367179d6b9cfc63c7a502e0a3b24ae
webQbe <webcubesolutionslk@gmail.com>
2024-11-05 19:42:11 +0530

    Removed line 'const { response } = require(express);'

--- public/client.js
+++ public/client.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 // Options
 const CLIENT_ID = '610977770552-2reklne551tb7n57dohvfrksmcf865se.apps.googleusercontent.com';
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
